3f81f645b55c4b5ff1be1c8c21946f899c6c2f57
Anders Fischer-Nielsen <andersfischern@me.com>
2025-12-23 20:12:17 +0100

    Add rate limits back

--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
-// import { rateLimiter } from "./rate-limiter";
+import { rateLimiter } from "./rate-limiter";
 import reasons from "./reasons.json";
 
-// const limiter = rateLimiter();
-// const error = "Too many requests, please try again later. (120 reqs/min/IP)";
+const limiter = rateLimiter();
+const error = "Too many requests, please try again later. (120 reqs/min/IP)";
 
 const server = Bun.serve({
   port: process.env.PORT || 3000,
   routes: {
     "/no": (req, server) => {
-      // const ip = server.requestIP(req)?.address ?? "unknown";
+      const ip = server.requestIP(req)?.address ?? "unknown";
 
-      // if (!limiter.get(ip)) {
-      //   return Response.json({ error });
-      // }
+      if (!limiter.get(ip)) {
+        return Response.json({ error });
+      }
 
       const reason = reasons[Math.floor(Math.random() * reasons.length)];
       return Response.json({ reason });
